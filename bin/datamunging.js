3d28dc2c27cfb3acebbbd0c8ead09f18aab5dbdb
Nimer <nimerabraham@gmail.com>
2017-09-25 20:36:16 -0600

    Fixed Bin

--- bin/datamunging.js
+++ bin/datamunging.js
@@ -6,12 +6,12 @@
  */
 
 /* eslint no-console:off */
-
+const path = require('path');
 const DataMunging = require('../src/index.js');
 
 const choice = process.argv[2];
 if (choice === 'weather') {
-  console.log(DataMunging.weatherData('./weather.dat'));
+  console.log(DataMunging.weatherData(path.join(__dirname, '../weather.dat')));
 } else {
-  console.log(DataMunging.footballData('./football.dat'));
+  console.log(DataMunging.footballData(path.join(__dirname, '../football.dat')));
 }
